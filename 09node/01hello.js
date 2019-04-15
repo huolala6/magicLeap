99014449412ff986b853165b7e50f9d7d9ab08fe
胡娜娜 <hnn@shv.im>
2019-04-15 16:20:24 +0800

    一些测试立体

---
+++ 09node/01hello.js
@@ -0,0 +1,105 @@
+'use strict'
+var s='hello'
+function greet(name){
+
+    console.log(s+':'+name+'!')
+}
+function hello(){
+    console.log(s+
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        '!')
+
+}
+module.exports={
+    greet:greet,
+    hello:hello
+}
---
+++ 09node/02main.js
@@ -0,0 +1,6 @@
+'use strict'
+let greet=require('./01hello');
+let s='main'
+greet.greet(s)
+console.log("main")
+
